f4f9b866a6c1331da4d3e4ad98d4a452ad8ab9a1
MateoMonastra <mateomonastravm@gmail.com>
2023-09-18 11:25:24 -0300

    resurrection of the project

--- Algebra1_TP2/Algebra1_TP2/main.cpp
+++ Algebra1_TP2/Algebra1_TP2/main.cpp
@@ -1,29 +1,112 @@
 #include "raylib.h"
+#include <iostream>
 #include "raymath.h"
+
+using namespace std;
+
+void values(Vector3& A, Vector3& B, Vector3& C, int vectorLenght, int aAxis, float cLength);
+
 int main()
 {
+	Vector3 A;
+	Vector3 B;
+	Vector3 C;
+	Vector3 ABC = { 0, 0, 0 };
 
-    InitWindow(800, 450, "Algebra1_TP2");
+	int userInput = 0;
+	int vectorLength = GetRandomValue(10, 100); //Largo de A y B
+	int aAxis = GetRandomValue(1, 3); //Si es X, Y o Z
+	float cLength = 1.0f / userInput * vectorLength;
 
-    float tamanioW = GetRandomValue(400, 700);
-    float tamanioH = GetRandomValue(100, 400);
+	cin >> userInput;
 
-    Vector2 start = { GetScreenWidth() / 6, GetScreenHeight() - 100 };
-    Vector2 linea = { 400, 350 };
-    Vector2 linea2 = { GetScreenHeight() - 400, (GetScreenWidth()/6 - 400 ) *-1};
+	values(A, B, C, vectorLength, aAxis, cLength);
 
-    /*Vector2 linea2 = Vector2Invert(linea);*/
+	const int screenWidth = 1366;
+	const int screenHeight = 768;
 
-    while (!WindowShouldClose())    
-    {
-        BeginDrawing();
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera mode");
 
-        ClearBackground(RAYWHITE);
+	Camera3D camera = { 0 };
+	camera.position = { 0.0f, 10.0f, 10.0f };  // Camera position
+	camera.target = { 0.0f, 0.0f, 0.0f };      // Camera looking at point
+	camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+	camera.fovy = 45.0f;                       // Camera field-of-view Y
+	camera.projection = CAMERA_PERSPECTIVE;    // Camera mode type
 
-        DrawLineV(start, linea, RED);
-        DrawLineV(start, linea2, BLUE);
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
 
-        EndDrawing();
-    }
-    return 0;
+		ClearBackground(RAYWHITE);
+
+		BeginMode3D(camera);
+
+		DrawLine3D(ABC, A, BLACK);
+		DrawLine3D(ABC, B, BLACK);
+		DrawLine3D(ABC, C, BLACK);
+
+		DrawGrid(10, 1.0f);
+
+		EndMode3D();
+
+		DrawFPS(10, 10);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+}
+
+void values(Vector3& A, Vector3& B, Vector3& C, int vectorLength, int aAxis, float cLength)
+{
+	switch (aAxis)
+	{
+	case 1:
+		A.x = vectorLength;
+		A.y = 0;
+		A.z = 0;
+		break;
+	case 2:
+		A.x = 0;
+		A.y = vectorLength;
+		A.z = 0;
+		break;
+	case 3:
+		A.x = 0;
+		A.y = 0;
+		A.z = vectorLength;
+		break;
+	}
+
+	if (A.x == vectorLength)
+	{
+		B.x = 0;
+		B.y = vectorLength;
+		B.z = 0;
+
+		C.x = 0;
+		C.y = 0;
+		C.z = cLength;
+	}
+	else if (A.y == vectorLength)
+	{
+		B.x = vectorLength;
+		B.y = 0;
+		B.z = 0;
+
+		C.x = 0;
+		C.y = 0;
+		C.z = cLength;
+	}
+	else if (A.z == vectorLength)
+	{
+		B.x = 0;
+		B.y = vectorLength;
+		B.z = 0;
+
+		C.x = cLength;
+		C.y = 0;
+		C.z = 0;
+	}
 }
