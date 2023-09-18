09fccc9c2fa241f799bc8e822703325092e99b01
Agonalix <santiago.seara853@gmail.com>
2023-09-18 18:08:44 -0300

    commented code
    comente el codigo para que sea mas legible

--- Algebra1_TP2/Algebra1_TP2/main.cpp
+++ Algebra1_TP2/Algebra1_TP2/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "raylib.h"
 #include "raymath.h"
+
 using namespace std;
 
+//Switch value random starter
 enum class axis
 {
 	AxisX = 1,
@@ -23,19 +25,23 @@ int main()
 	int vectorLength = GetRandomValue(10, 10); //Largo de A y B
 	int aAxis = 1/*GetRandomValue(1, 3)*/; //Si es X, Y o Z
 
+	cout << "Por cuanto quieres que se divida el vector random? (El resultado de este numero es la distancia del eje z)" << endl;
 	cin >> userInput;
 
 	float cLength = 1.0f / userInput * vectorLength;
 
-	values(firstLineA, firstLineB, firstLineC, vectorLength, aAxis, cLength);
 
 	const int screenWidth = 1366;
 	const int screenHeight = 768;
+	InitWindow(screenWidth, screenHeight, "TP2 Algebra");
+
+	values(firstLineA, firstLineB, firstLineC, vectorLength, aAxis, cLength);
+
 
 	SetTargetFPS(144);
 
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera mode");
-
+	//----------------------------------------------------------------------------------------------
+	//Camera in 3D
 	Camera3D camera = { 0 };
 	camera.position = { 0.0f, 10.0f, 10.0f };  // Camera position
 	camera.target = { 0.0f, 0.0f, 0.0f };      // Camera looking at point
@@ -45,38 +51,45 @@ int main()
 
 	DisableCursor();
 
-	cout << vectorLength;
-
 	while (!WindowShouldClose())
 	{
-		UpdateCamera(&camera, CAMERA_FREE);
+		//------------------------------------------------------------------------------------------
+		//UPDATE
+
+
+		UpdateCamera(&camera, CAMERA_FREE); // camera movement
 
 		if (IsKeyDown('Z')) camera.target = { 0.0f, 0.0f, 0.0f };
 
+		//------------------------------------------------------------------------------------------
+		//DRAWING
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
 
 		BeginMode3D(camera);
 
+
+
+		//Basic lines, A, B and C
 		DrawLine3D(Zero, firstLineA, RED);
 		DrawLine3D(Zero, firstLineB, GREEN);
 		DrawLine3D(Zero, firstLineC, BLUE);
 
-		for (int i = 0; i < 5; i++)
-		{
-			DrawLine3D(Zero, firstLineA, BROWN);
-			DrawLine3D(Zero, firstLineB, BROWN);
-			DrawLine3D(firstLineA, Vector3Add(firstLineA, firstLineB), BROWN);
-			DrawLine3D(firstLineB, Vector3Add(firstLineA, firstLineB), BROWN);
+		Vector3 prueba = Vector3Add(firstLineA, firstLineB);
 
+		// Pyramid base lines
+		DrawLine3D(firstLineA, Vector3Add(firstLineA, firstLineB), BLACK);
+		DrawLine3D(firstLineB, Vector3Add(firstLineA, firstLineB), BLACK);
 
-		}
+		DrawLine3D(firstLineC, Vector3Add(firstLineA, firstLineC), BLACK);
+		DrawLine3D(firstLineC, Vector3Add(firstLineB, firstLineC), BLACK);
+		DrawLine3D(Vector3Add(firstLineC, prueba), Vector3Add(firstLineA, firstLineC), BLACK);
+		DrawLine3D(Vector3Add(firstLineC, prueba), Vector3Add(firstLineB, firstLineC), BLACK);
 
-		/*DrawLine3D(Vector3AddValue(ABC, cLength), firstLineA, BROWN);
-		DrawLine3D(Vector3AddValue(ABC, cLength), firstLineB, BROWN);
-		DrawLine3D(Vector3AddValue(firstLineA, cLength), Vector3Add(firstLineA, firstLineB), BROWN);
-		DrawLine3D(Vector3AddValue(firstLineB, cLength), Vector3Add(firstLineA, firstLineB), BROWN);*/
+		DrawLine3D(firstLineA, Vector3Add(firstLineA, firstLineC), BLACK);
+		DrawLine3D(firstLineB, Vector3Add(firstLineB, firstLineC), BLACK);
+		DrawLine3D(Vector3Add(firstLineA, firstLineB), Vector3Add(firstLineC, prueba), BLACK);
 
 		EndMode3D();
 
@@ -88,62 +101,55 @@ int main()
 	CloseWindow();
 }
 
-void values(Vector3& A, Vector3& B, Vector3& C, int vectorLength, int aAxis, float cLength)
+void values(Vector3& firstLineA, Vector3& firstLineB, Vector3& firstLineC, int vectorLength, int aAxis, float cLength)
 {
+	// Based on what the value of A is, the program gives B and C a value on x,y,z
 	switch (aAxis)
 	{
 	case (int)axis::AxisX:
+		firstLineA.x = vectorLength;
+		firstLineA.y = 0;
+		firstLineA.z = 0;
 
-		A.x = vectorLength;
-		A.y = 0;
-		A.z = 0;
+		firstLineB.x = 0;
+		firstLineB.y = vectorLength;
+		firstLineB.z = 0;
 
-		B.x = 0;
-		B.y = vectorLength;
-		B.z = 0;
-
-		C.x = 0;
-		C.y = 0;
-		C.z = cLength;
-
-
+		firstLineC.x = 0;
+		firstLineC.y = 0;
+		firstLineC.z = cLength;
 		break;
 
 	case (int)axis::AxisY:
+		firstLineA.x = 0;
+		firstLineA.y = vectorLength;
+		firstLineA.z = 0;
 
-		A.x = 0;
-		A.y = vectorLength;
-		A.z = 0;
+		firstLineB.x = vectorLength;
+		firstLineB.y = 0;
+		firstLineB.z = 0;
 
-		B.x = vectorLength;
-		B.y = 0;
-		B.z = 0;
-
-		C.x = 0;
-		C.y = 0;
-		C.z = cLength;
-
+		firstLineC.x = 0;
+		firstLineC.y = 0;
+		firstLineC.z = cLength;
 		break;
 
 	case (int)axis::AxisZ:
+		firstLineA.x = 0;
+		firstLineA.y = 0;
+		firstLineA.z = vectorLength;
 
-		A.x = 0;
-		A.y = 0;
-		A.z = vectorLength;
+		firstLineB.x = 0;
+		firstLineB.y = vectorLength;
+		firstLineB.z = 0;
 
-		B.x = 0;
-		B.y = vectorLength;
-		B.z = 0;
-
-		C.x = cLength;
-		C.y = 0;
-		C.z = 0;
-
+		firstLineC.x = cLength;
+		firstLineC.y = 0;
+		firstLineC.z = 0;
 		break;
+
 	default:
+
 		break;
 	}
-
-
-
 }
